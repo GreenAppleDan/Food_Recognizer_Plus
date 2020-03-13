fe7926bf8dd7ec3055cd0f563b7cef860cab5e5a
Denis Zhukoborsky <d.jukoborskiy@key-g.com>
2020-03-13 18:05:40 +0300

    added recipesViewController

---
+++ Food Recognizer+/UI/Screens/RecipesViewController/RecipesViewControllerProtocol.swift
@@ -0,0 +1,13 @@
+//
+//  RecipesViewControllerProtocol.swift
+//  Food Recognizer+
+//
+//  Created by dev on 13.03.2020.
+//  Copyright © 2020 Денис Жукоборский. All rights reserved.
+//
+
+import Foundation
+
+protocol RecipesViewControllerProtocol: ViewControllerProtocol {
+    
+}
